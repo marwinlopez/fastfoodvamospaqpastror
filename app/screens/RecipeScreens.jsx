fe566e118e3d498717f72afad017b7c9282f5d4b
Marwin Lopez <marwin.lopez@example.com>
2026-06-27 22:16:41 -0400

    style: refactor RecipeScreens to apply Canvas Design System and remove easy-grid dependency

--- app/screens/RecipeScreens.jsx
+++ app/screens/RecipeScreens.jsx
@@ -1,16 +1,14 @@
-import React, { useState, useEffect, useReducer } from "react";
+import React, { useEffect, useReducer } from "react";
 import {
-  Dimensions,
   FlatList,
-  Pressable,
   SafeAreaView,
+  StatusBar,
+  StyleSheet,
   Text,
   TouchableOpacity,
   View,
 } from "react-native";
-import { Col, Grid, Row } from "react-native-easy-grid";
 import Header from "../components/Header";
-import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 import NebulaTextInput from "../src/components/NebulaTextInput";
 import { COLORS } from "../src/constants/themes";
 import RecipeReducer, {
@@ -18,19 +16,11 @@ import RecipeReducer, {
   initialState,
 } from "../hooks/RecipeReducer";
 import { Button } from "@rneui/themed";
-import LinkItemList from "../components/LinkItemList";
 import apis from "../apis";
+import { Feather } from "@expo/vector-icons";
 
 const RecipeScreens = ({ navigation, route }) => {
-  var { height } = Dimensions.get("window");
   const [state, dispatch] = useReducer(RecipeReducer, initialState);
-  const [stateButtonAdd, setStateButtonAdd] = useState(true);
-
-  // useEffect(() => {
-  //   const { isAddQuantity, loading, recipe, isShowDetails } = state;
-  //   const { ingredients } = recipe;
-  //   console.log(recipe);
-  // }, [state]);
 
   useEffect(() => {
     const unsubscribe = navigation.addListener("focus", () => {
@@ -38,10 +28,7 @@ const RecipeScreens = ({ navigation, route }) => {
       const { recipe } = route.params;
       switch (route.params.route) {
         case "editRecipe":
-          console.log(recipe);
-          // dispatch(actionCreators.success(recipe));
           actionCreators.editRecipe(recipe).then((data) => {
-            console.log(data);
             dispatch(actionCreators.success(data));
           });
           break;
@@ -52,7 +39,6 @@ const RecipeScreens = ({ navigation, route }) => {
         case "newRecipe":
           dispatch(actionCreators.loading());
           if (recipe) dispatch(actionCreators.success(recipe));
-
           break;
         default:
           console.log("no existe parametro");
@@ -62,8 +48,8 @@ const RecipeScreens = ({ navigation, route }) => {
     return unsubscribe;
   }, []);
 
-  const { isAddQuantity, loading, recipe, isShowDetails } = state;
-  // const { ingredients } = recipe;
+  const { isAddQuantity, recipe } = state;
+
   const redirectActionLeft = () => {
     navigation.push("RecipesScreen", { recipe: recipe });
   };
@@ -75,211 +61,305 @@ const RecipeScreens = ({ navigation, route }) => {
     });
   };
 
-  const onChange = () => {};
-
   const addIngredients = () => {
     navigation.navigate("MaterialsScreen", { recipe: recipe });
   };
 
   return (
-    <SafeAreaView style={{ height: "100%" }}>
+    <SafeAreaView style={styles.container}>
+      <StatusBar barStyle="dark-content" backgroundColor="#FAFAFA" />
       <Header
         title={"Recetas"}
         buttonLeft={"arrow-left"}
         actionLeft={redirectActionLeft}
         isSearch={true}
       />
-      <Grid>
-        <Row style={{ height: 120 }}>
-          <Col
-            style={{
-              flex: 1.5,
-              justifyContent: "flex-start",
-              padding: 5,
+
+      <View style={styles.content}>
+        {/* Formulario Nombre Receta */}
+        <View style={styles.formGroup}>
+          <Text style={styles.label}>Nombre de la Receta</Text>
+          <NebulaTextInput
+            defaultValue={recipe?.name}
+            onChangeText={(text) => {
+              dispatch(actionCreators.changeName(text.trim()));
             }}
-          >
-            <Row style={{ height: 25 }}>
-              <Text
-                style={{
-                  color: COLORS.white,
-                  fontSize: 18,
-                  fontWeight: "900",
+            placeholder="Ej. Hamburguesa Doble"
+          />
+        </View>
+
+        {/* Acciones de Edición/Adición */}
+        <View style={styles.actionsRow}>
+          {recipe?.id ? (
+            <>
+              <Button
+                containerStyle={styles.buttonContainer}
+                disabled={isAddQuantity}
+                buttonStyle={styles.buttonStyle}
+                title="Editar Nombre"
+                titleStyle={styles.buttonTitle}
+                icon={{
+                  name: "edit-2",
+                  type: "feather",
+                  size: 16,
+                  color: "white",
                 }}
-              >
-                Nombre de la Receta:
-              </Text>
-            </Row>
-            <Row style={{ height: 40 }}>
-              <Col size={1}>
-                <NebulaTextInput
-                  defaultValue={recipe?.name}
-                  onChangeText={(text) => {
-                    dispatch(actionCreators.changeName(text.trim()));
-                  }}
-                />
-              </Col>
-            </Row>
-            <Row style={{ paddingTop: 5, height: 60 }}>
-              <Col
-                style={{
-                  flex: 1,
-                  justifyContent: "center",
-                }}
-              >
-                <Row>
-                  {recipe?.id ? (
-                    <>
-                      <Col>
-                        <Button
-                          containerStyle={{
-                            paddingLeft: 0,
-                            borderRadius: 5,
-                            alignContent: "center",
-                          }}
-                          disabled={isAddQuantity}
-                          buttonStyle={{
-                            backgroundColor: COLORS.default,
-                          }}
-                          title="Editar Nombre"
-                          icon={{
-                            name: "save",
-                            type: "font-awesome",
-                            size: 18,
-                            color: "white",
-                          }}
-                          iconContainerStyle={{ marginRight: 5 }}
-                          onPress={addIngredients}
-                        />
-                      </Col>
-                      <Col style={{ width: 5 }} />
-                    </>
-                  ) : null}
-                  <Col>
-                    <Button
-                      containerStyle={{
-                        paddingLeft: 0,
-                        borderRadius: 5,
-                        alignContent: "center",
-                      }}
-                      disabled={isAddQuantity}
-                      buttonStyle={{
-                        backgroundColor: COLORS.default,
-                      }}
-                      title="Añadir Cantidades"
-                      icon={{
-                        name: "plus",
-                        type: "font-awesome",
-                        size: 18,
-                        color: "white",
-                      }}
-                      iconContainerStyle={{ marginRight: 5 }}
-                      onPress={addIngredients}
-                    />
-                  </Col>
-                </Row>
-              </Col>
-            </Row>
-          </Col>
-        </Row>
-        <Row
-          style={{
-            height: height - 240,
-            paddingVertical: 5,
-            paddingHorizontal: 5,
-          }}
-        >
-          <Col
-            style={{
-              // height: 600,
-              borderColor: COLORS.default,
-              paddingHorizontal: 1,
-              borderWidth: 1,
-              borderRadius: 5,
-            }}
-          >
-            {isShowDetails ? null : (
-              <FlatList
-                style={{ flex: 1 }}
-                scrollEnabled
-                // keyExtractor={(ingredient) => ingredient.idIngredient}
-                data={recipe?.ingredients}
-                ItemSeparatorComponent={() => <View style={{ padding: 2 }} />}
-                renderItem={({ item }) => (
-                  <TouchableOpacity key={`item-${item.idIngredient}`}>
-                    <LinkItemList
-                      id={item.idIngredient}
-                      name={`${item.description}`}
-                      quantity={`${item.quantity} ${item.unitOfMeasurement}`}
-                      cost={`${item.cost}`}
-                      coin={item.coin}
-                      url="Material"
-                    />
-                  </TouchableOpacity>
-                )}
-                numColumns={1}
+                iconContainerStyle={{ marginRight: 6 }}
+                onPress={addIngredients}
               />
-            )}
-          </Col>
-        </Row>
-        <Row style={{ height: 100 }}>
-          <Col style={{ width: "40%" }} />
-          <Col>
-            <Row style={{ height: 20 }}>
-              <Col style={{ alignItems: "flex-end", paddingRight: 10 }}>
-                <Text>{`Costo ${recipe?.coin}: `}</Text>
-              </Col>
-              <Col
-                style={{
-                  backgroundColor: COLORS.default,
-                  borderRadius: 5,
-                  marginRight: 5,
-                  alignItems: "flex-end",
-                }}
-              >
-                <Text style={{ marginHorizontal: 5 }}>{`${recipe?.cost}`}</Text>
-              </Col>
-            </Row>
-            <Row style={{ height: 1 }} />
-            <Row style={{ height: 20 }}>
-              <Col style={{ alignItems: "flex-end", paddingRight: 10 }}>
-                <Text>Ganancia %: </Text>
-              </Col>
-              <Col
-                style={{
-                  backgroundColor: COLORS.default,
-                  borderRadius: 5,
-                  marginRight: 5,
-                  alignItems: "flex-end",
-                }}
-              >
-                <Text
-                  style={{ marginHorizontal: 5 }}
-                >{`${recipe?.profit}`}</Text>
-              </Col>
-            </Row>
-            <Row style={{ height: 1 }} />
-            <Row style={{ height: 20 }}>
-              <Col style={{ alignItems: "flex-end", paddingRight: 10 }}>
-                <Text>{`Precio ${recipe?.coin}: `}</Text>
-              </Col>
-              <Col
-                style={{
-                  backgroundColor: COLORS.default,
-                  borderRadius: 5,
-                  marginRight: 5,
-                  alignItems: "flex-end",
-                }}
-              >
-                <Text
-                  style={{ marginHorizontal: 5 }}
-                >{`${recipe?.price}`}</Text>
-              </Col>
-            </Row>
-          </Col>
-        </Row>
-      </Grid>
+              <View style={{ width: 12 }} />
+            </>
+          ) : null}
+          <Button
+            containerStyle={styles.buttonContainer}
+            disabled={isAddQuantity}
+            buttonStyle={styles.buttonStyle}
+            title="Añadir Cantidades"
+            titleStyle={styles.buttonTitle}
+            icon={{
+              name: "plus",
+              type: "feather",
+              size: 16,
+              color: "white",
+            }}
+            iconContainerStyle={{ marginRight: 6 }}
+            onPress={addIngredients}
+          />
+        </View>
+
+        {/* Listado de Ingredientes */}
+        <Text style={styles.sectionTitle}>Ingredientes</Text>
+        <FlatList
+          style={styles.list}
+          contentContainerStyle={styles.listContent}
+          data={recipe?.ingredients}
+          keyExtractor={(item, index) => index.toString()}
+          ListEmptyComponent={() => (
+            <View style={styles.emptyContainer}>
+              <Feather name="layers" size={32} color="#8E9AA6" />
+              <Text style={styles.emptyText}>Sin ingredientes añadidos</Text>
+            </View>
+          )}
+          renderItem={({ item }) => (
+            <View style={styles.ingredientCard}>
+              <View style={styles.cardLeft}>
+                <View style={styles.bulletIconWrapper}>
+                  <Feather name="layers" size={16} color="#5802F1" />
+                </View>
+                <View style={styles.textContent}>
+                  <Text style={styles.ingredientName}>{item.description}</Text>
+                  <Text style={styles.ingredientQty}>
+                    Cantidad: {item.quantity} {item.unitOfMeasurement}
+                  </Text>
+                </View>
+              </View>
+              <View style={styles.cardRight}>
+                <View style={styles.costBadge}>
+                  <Text style={styles.costText}>
+                    {item.coin || "USD"} {item.cost}
+                  </Text>
+                </View>
+              </View>
+            </View>
+          )}
+          showsVerticalScrollIndicator={false}
+        />
+
+        {/* Panel Financiero Resumen (Dashboard) */}
+        <View style={styles.financialCard}>
+          <View style={styles.metricColumn}>
+            <Text style={styles.metricLabel}>Costo ({recipe?.coin || "USD"})</Text>
+            <Text style={styles.metricValue}>{recipe?.cost || "0.00"}</Text>
+          </View>
+          <View style={styles.metricDivider} />
+          <View style={styles.metricColumn}>
+            <Text style={styles.metricLabel}>Ganancia</Text>
+            <Text style={styles.metricValue}>{recipe?.profit || "0"}%</Text>
+          </View>
+          <View style={styles.metricDivider} />
+          <View style={styles.metricColumn}>
+            <Text style={styles.metricLabel}>Precio ({recipe?.coin || "USD"})</Text>
+            <Text style={[styles.metricValue, styles.priceValue]}>
+              {recipe?.price || "0.00"}
+            </Text>
+          </View>
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FAFAFA", // Fondo Canvas
+  },
+  content: {
+    flex: 1,
+    paddingHorizontal: 20,
+    paddingTop: 16,
+  },
+  formGroup: {
+    marginBottom: 20,
+  },
+  label: {
+    fontSize: 12,
+    fontWeight: "700",
+    color: "#8E9AA6",
+    textTransform: "uppercase",
+    letterSpacing: 1.2,
+    marginBottom: 8,
+  },
+  actionsRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 24,
+  },
+  buttonContainer: {
+    flex: 1,
+    borderRadius: 14,
+    overflow: "hidden",
+  },
+  buttonStyle: {
+    backgroundColor: "#5802F1", // Morado de marca
+    paddingVertical: 12,
+    borderRadius: 14,
+  },
+  buttonTitle: {
+    fontSize: 14,
+    fontWeight: "700",
+  },
+  sectionTitle: {
+    fontSize: 12,
+    fontWeight: "700",
+    color: "#8E9AA6",
+    textTransform: "uppercase",
+    letterSpacing: 1.2,
+    marginBottom: 12,
+  },
+  list: {
+    flex: 1,
+  },
+  listContent: {
+    paddingBottom: 16,
+  },
+  emptyContainer: {
+    paddingVertical: 32,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#FFFFFF",
+    borderRadius: 20,
+    shadowColor: "#1A1D20",
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.02,
+    shadowRadius: 8,
+    elevation: 1,
+  },
+  emptyText: {
+    fontSize: 13,
+    color: "#8E9AA6",
+    marginTop: 10,
+    fontWeight: "600",
+  },
+  ingredientCard: {
+    backgroundColor: "#FFFFFF",
+    borderRadius: 16,
+    padding: 14,
+    marginVertical: 6,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    // Sombra suave Canvas
+    shadowColor: "#1A1D20",
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.02,
+    shadowRadius: 8,
+    elevation: 2,
+  },
+  cardLeft: {
+    flexDirection: "row",
+    alignItems: "center",
+    flex: 1,
+  },
+  bulletIconWrapper: {
+    width: 36,
+    height: 36,
+    borderRadius: 10,
+    backgroundColor: "#5802F110",
+    justifyContent: "center",
+    alignItems: "center",
+    marginRight: 12,
+  },
+  textContent: {
+    flex: 1,
+  },
+  ingredientName: {
+    fontSize: 14,
+    fontWeight: "700",
+    color: "#1A1D20",
+    marginBottom: 2,
+  },
+  ingredientQty: {
+    fontSize: 11,
+    color: "#8E9AA6",
+    fontWeight: "600",
+  },
+  cardRight: {
+    justifyContent: "center",
+    alignItems: "flex-end",
+  },
+  costBadge: {
+    backgroundColor: "#F3F4F6", // Fondo gris suave neutral
+    borderRadius: 8,
+    paddingHorizontal: 8,
+    paddingVertical: 4,
+  },
+  costText: {
+    fontSize: 11,
+    fontWeight: "700",
+    color: "#4B5563",
+  },
+  financialCard: {
+    backgroundColor: "#FFFFFF",
+    borderRadius: 20,
+    padding: 16,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginTop: 12,
+    marginBottom: 20,
+    // Sombras marcadas pero sutiles
+    shadowColor: "#1A1D20",
+    shadowOffset: { width: 0, height: 8 },
+    shadowOpacity: 0.04,
+    shadowRadius: 16,
+    elevation: 3,
+  },
+  metricColumn: {
+    flex: 1,
+    alignItems: "center",
+  },
+  metricLabel: {
+    fontSize: 10,
+    color: "#8E9AA6",
+    fontWeight: "700",
+    textTransform: "uppercase",
+    letterSpacing: 0.5,
+    marginBottom: 4,
+  },
+  metricValue: {
+    fontSize: 16,
+    fontWeight: "800",
+    color: "#1A1D20",
+  },
+  priceValue: {
+    color: "#137333", // Verde destacado para el precio de venta final
+  },
+  metricDivider: {
+    width: 1,
+    height: 30,
+    backgroundColor: "#F3F4F6",
+  },
+});
 
 export default RecipeScreens;
